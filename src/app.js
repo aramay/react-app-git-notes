8b9b6655cb30dd2f82febbae5160736985a2ca96
“kasper341” <kasper_341@hotmail.com>
2017-01-29 23:21:53 -0800

    remove redundant files

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
-document.write('Hello Frontend Masters!')
+import Router from './router'
+import styles from './styles/main.css'
 
+window.app = {
+    init () {
+        this.router = new Router()
+        // console.log(this.router)
+        this.router.history.start()
+    }
+}
+
+window.app.init()
